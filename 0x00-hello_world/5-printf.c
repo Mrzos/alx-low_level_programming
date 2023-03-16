36fded9de6878f36c3fe059e8c3fef0dbbf280f3
BOUILFANI MOHAMED SAID <Saidzos997@gmail.com>
2023-03-16 16:44:46 -0700

    Hello, printf TASL

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+
+/*this is the firs time we can use frintf function */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
