775236a863df93d24577ebce7144bacfa04c73a1
BOUILFANI MOHAMED SAID <Saidzos997@gmail.com>
2023-03-16 16:29:15 -0700

    Hello,puts TASK

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 int main()
 {
-	char str1[52] = "\"Programming is like building a multilingual puzzle");
+	char str1[52] = "\"Programming is like building a multilingual puzzle";
 	
 	puts(str1);
 
