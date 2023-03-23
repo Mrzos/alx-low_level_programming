d14a2d414c16a725c593240a436c8deb292dca46
BOUILFANI MOHAMED SAID <Saidzos997@gmail.com>
2023-03-22 17:05:22 -0700

    alphABET TASK

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -3,23 +3,23 @@
 /**
  * main - is to type alphabitic from A to Z
  *
- * Return : always 0 (success)
+ * Return: always 0 (success)
  */
 int main(void)
 {
         char n = 'a';
         char m = 'A';
 
-        while ( n <= 122)
-        {
-                putchar(n);
-                n++;
-        }
-        while ( m <= 90)
-        {
-                putchar(m);
-                m++;
-        }
-        putchar('\n');
-        return (0);
+	while (n<=122)
+	{
+		putchar(n);
+		n++;
+	}
+	while (m<=90)
+	{
+		putchar(m);
+		m++;
+	}
+	putchar('\n');
+	return (0);
 }
