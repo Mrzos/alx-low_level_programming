d505933eccc42c6d29889c263d5ce3c3c4f32a99
BOUILFANI MOHAMED SAID <Saidzos997@gmail.com>
2023-03-22 17:40:16 -0700

    Numbers TASK

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 
 /*
- * main - this is to type munbers from 1 to 9 
+ * main - this is to type munbers from 1 to 9
  *
  * Return: always 0 (seccuss)
- * */
+ **/
 int main(void)
 {
-	printf("0123456789");
+	printf("0123456789\n");
 	return (0);
 }
