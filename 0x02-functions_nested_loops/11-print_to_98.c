2a1542783143f7bb40187f387df858be510643b0
BOUILFANI MOHAMED SAID <Saidzos997@gmail.com>
2023-03-24 15:41:42 -0700

    print ri 98 TASK

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+
+/**
+ * print_to_98 - Prints all natural numbers from input to 98,
+ *               in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
+ */
+
+#include "6-abs.c"
+#include <stdio.h>
+
+void print_to_98(int n)
+{
+	int i;
+
+	if (n > 98)
+	{
+		for (i = n; i >= 98; i--)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	} else
+	{
+		for (i = n; i <= 98; i++)
+		{
+			printf("%d", i);
+			if (i != 98)
+			{
+				printf(", ");
+			}
+		}
+	}
+	printf("\n");
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -9,4 +9,5 @@ void times_table(void);
 int add(int, int);
 int print_last_digit(int);
 void jack_bauer(void);
+void print_to_98(int n);
 
