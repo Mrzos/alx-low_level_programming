be3114c5a9ccd829940f93cc71df8f0ec877d831
BOUILFANI MOHAMED SAID <Saidzos997@gmail.com>
2023-03-24 11:43:09 -0700

    absolute TASK

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_sign - main function
+ *
+ * @n: character to compare
+ *
+ * Return: 1 if positive, 0 if zero, -1 if negative
+ *
+ */
+
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return(n);
+	
+	}
+	else 
+	{
+		return(-n);
+	}
+}
