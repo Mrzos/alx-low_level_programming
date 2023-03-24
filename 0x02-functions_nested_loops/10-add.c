589055f741788579ca588f25c73b0b8b2615583b
BOUILFANI MOHAMED SAID <Saidzos997@gmail.com>
2023-03-24 14:51:34 -0700

    add TASK

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,12 +1,13 @@
 #include "main.h"
 
 /*
- * int add - adds two integers and returns the result.
+ * add - adds two integers and returns the result.
  *
  * Return: 0.
  */
 int add(int a, int b)
 {
 	int sum = a + b;
-	return(sum);
+
+	return (sum);
 }
