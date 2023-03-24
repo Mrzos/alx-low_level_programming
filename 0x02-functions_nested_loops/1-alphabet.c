1bb6e2de51de890597e4b08532db561b885d79db
BOUILFANI MOHAMED SAID <Saidzos997@gmail.com>
2023-03-23 17:08:18 -0700

    alphabitic TASK

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,14 +1,14 @@
 #include "main.h"
 
 /**
- * main - printing alphabitics using lowercase 
+ * main - printing alphabitics using lowercase
  *
  */
 void print_alphabet(void)
 {
 	char c;
 
-	for ( c = 'a'; c <= 'z' ; c++ )
+	for (c = 'a'; c <= 'z' ; c++)
 	{
 		_putchar(c);
 	}
