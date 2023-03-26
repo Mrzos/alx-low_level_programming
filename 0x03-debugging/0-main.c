19cf2a9d0372c196bf24f25466dfa4b08b56674e
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-03-26 06:45:42 -0700

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * main - Test function for positive or negative
- * Return: 0.
+ * main - is to test debugging
+ * @i - is a variable
+ * Return: 0
  */
-
 int main(void)
 {
 	int i;
