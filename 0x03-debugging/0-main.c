2226801438b6b5b84b3af36ab0d05540f7272402
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-03-26 06:22:57 -0700

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
 #include <stdlib.h>
 #include <time.h>
 #include "main.h"
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+/**
+ * main - is to test debugging
+ *
+ * Return: 0
+ */
 int main(void)
 {
 	int n;
 	
 	n = 0;
-
-	/* your code goes there */
 	positive_or_negative(n);
 	return (0);
 }
