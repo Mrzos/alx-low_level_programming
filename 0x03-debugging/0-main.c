883ae4de37b084825a6731aa1dabd893383df08e
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-03-26 06:27:50 -0700

    multiple mains

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -3,13 +3,13 @@
 /**
  * main - is to test debugging
  *
- * @n: is a variable
+ * @n - is a variable
  * Return: 0
  */
 int main(void)
 {
 	int n;
-	
+
 	n = 0;
 	positive_or_negative(n);
 	return (0);
