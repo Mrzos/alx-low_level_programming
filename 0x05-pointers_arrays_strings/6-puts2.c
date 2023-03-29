4afbba671e1169f5ce1c04b4032a11c949e443c3
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-03-29 13:34:54 -0700

    puts2 TASK

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -11,7 +11,15 @@ void puts2(char *str)
 	while (*str != '\0')
 	{
 		_putchar(*str);
-		str += 2;
+		str++;
+		if (*str == 0)
+		{
+			break;
+		}
+		else if (*str != 0)
+		{
+			str++;
+		}
 	}
 	_putchar('\n');
 }
