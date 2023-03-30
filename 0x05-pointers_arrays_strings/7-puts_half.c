d4d45a72a30be595e6d31b08ac7bf0e82f8c2db0
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-03-29 17:37:32 -0700

    put half TASK

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,39 @@
+#include "main.h"
+
+/**
+ * puts_half - to tyepe  a string
+ *
+ * @str: a variable
+ * Return: 0
+ */
+void puts_half(char *str)
+{
+	int l = 0;
+	int half;
+	int h2;
+
+	while (str[l] != '\0')
+	{
+		l++;
+	}
+	half = l % 2;
+	if (half == 0)
+	{
+		h2 = l / 2;
+		while (str[h2] != '\0')
+		{
+			_putchar(str[h2]);
+			h2++;
+		}
+	}
+	else if (half == 0)
+	{
+		h2 = (l - 1) / 2;
+		while (str[h2] != '\0')
+		{
+			_putchar(str[h2]);
+			h2++;
+		}
+	}
+	_putchar('\n');
+}
