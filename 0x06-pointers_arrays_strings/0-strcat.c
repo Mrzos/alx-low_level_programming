5455c4848cd299e3ed9aa7ff976b3564b7cc0e75
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-04-01 10:37:46 -0700

    _strcat TASK

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -4,13 +4,12 @@
  * _strcat - to complet a array
  * @dest: is a variable
  * @src: also a variable
- * Return: 0
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
 	int i = 0;
 	int len = 0;
-	int n = 0;
 	int p;
 
 	while (dest[len] != '\0')
@@ -18,9 +17,10 @@ char *_strcat(char *dest, char *src)
 		len++;
 	}
 	p = len + i;
-	while (i < p && src[i] != '\0' )
+	while (i <= p && src[i] != '\0')
 	{
 		dest[len + i] = src[i];
 		i++;
 	}
+	return(0);
 }
