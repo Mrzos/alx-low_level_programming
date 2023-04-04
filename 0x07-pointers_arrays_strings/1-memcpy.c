42c326bf847264092c727c4a138e7b6acd804153
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-04-03 18:21:45 -0700

    _memcpy TASK

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -3,7 +3,7 @@
  * _memcpy - fills memory with a constant byte
  * @dest: the address of memory to print
  * @n: the size of the memory to print
- * @src: another variable 
+ * @src: another variable
  * Return: s.
  */
 char *_memcpy(char *dest, char *src, unsigned int n)
