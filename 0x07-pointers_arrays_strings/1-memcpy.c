0692ca30710ca35837739e805aab7bf4380b7c39
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-04-03 18:13:39 -0700

    _memcpy TASK

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * _memset - fills memory with a constant byte
+ * @s: the address of memory to print
+ * @n: the size of the memory to print
+ * @b: another variable
+ * Return: s.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int l = 0;
+
+	while (l < n)
+	{
+		dest[l] = src[l];
+		l++;
+	}
+	return (dest);
+}
