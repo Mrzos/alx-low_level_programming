a333c844457eb254e97c324047fdb8d7df613a3d
BOUILFANI MOHAMED SAID <saidzos997@gmail.com>
2023-04-08 05:22:04 -0700

    factorial TASK

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * factorial - prints the chessboard.
+ * @n: a variable
+ * Return: value.
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	else if (n == 0)
+	{
+		return (1);
+	}
+	return n * factorial(n - 1)
+}
